982366189cd31756eaad9846f2603ee31700cf59
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2022-10-12 19:38:47 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-const routes = [{ path: "/", exact: true, redirect: "/rate-doggos" }];
+const routes = [];
 
 const router = new VueRouter({
   mode: "history",
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // "transpileDependencies": [
+  //   "vuetify"
+  // ],
   lintOnSave: false,
   configureWebpack: {
     devServer: {
@@ -9,7 +12,37 @@ module.exports = {
       sockPort: 8501,
       sockHost: "localhost"
     },
+    // module: {
+    //   rules: [
+    //     {
+    //       test: /\.vue$/,
+    //       use: ["vue-loader"],
+    //     },
+    //     {
+    //       test: /\.scss$/,
+    //       exclude: /node_modules/,
+    //       use: [
+    //           {
+    //               loader: 'style-loader',
+    //           },
+    //           {
+    //               loader: 'css-loader',
+    //               options: {
+    //                   sourceMap: true,
+    //               },
+    //           },
+    //           {
+    //               loader: 'sass-loader',
+    //               options: {
+    //                   sourceMap: true,
+    //               },
+    //           },
+    //       ],
+    //     }
+    //   ],
+    // },
     externals: ["vue", "vue-router", /^@vue-mf\/.+/]
   },
   filenameHashing: false
 };
+
