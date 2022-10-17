b6e22bc187df6c6d96152b441e6fee0b3247c24f
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2022-10-17 14:44:23 +0700

    update config

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,15 +1,15 @@
-import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+import "material-design-icons-iconfont/dist/material-design-icons.css"; // Ensure you are using css-loader
 // import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-import Vue from 'vue'
-import Vuetify from 'vuetify'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 // import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
 const opts = {
-    icons: {
-        iconfont: 'mdi',
-    },
-}
+  icons: {
+    iconfont: "md",
+  },
+};
 
-export default new Vuetify(opts)
+export default new Vuetify(opts);
--- vue.config.js
+++ vue.config.js
@@ -1,48 +1,17 @@
 module.exports = {
-  // "transpileDependencies": [
-  //   "vuetify"
-  // ],
   lintOnSave: false,
   configureWebpack: {
     devServer: {
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        "Access-Control-Allow-Origin": "*",
       },
       disableHostCheck: true,
       sockPort: 8501,
-      sockHost: "localhost"
+      sockHost: "localhost",
+      https: true,
+      port: 8501,
     },
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.vue$/,
-    //       use: ["vue-loader"],
-    //     },
-    //     {
-    //       test: /\.scss$/,
-    //       exclude: /node_modules/,
-    //       use: [
-    //           {
-    //               loader: 'style-loader',
-    //           },
-    //           {
-    //               loader: 'css-loader',
-    //               options: {
-    //                   sourceMap: true,
-    //               },
-    //           },
-    //           {
-    //               loader: 'sass-loader',
-    //               options: {
-    //                   sourceMap: true,
-    //               },
-    //           },
-    //       ],
-    //     }
-    //   ],
-    // },
-    externals: ["vue", "vue-router", /^@vue-mf\/.+/]
+    externals: ["vue", "vue-router", /^@vue-mf\/.+/],
   },
-  filenameHashing: false
+  filenameHashing: false,
 };
-
