cc35e9a30255335acfa2c6aefb4709c40c321511
junior <junior@edenfarm.id>
2022-10-18 19:04:14 +0700

    adjust for home page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <div>
-    <Navbar v-show="navbarHandler" />
-    <v-main>
-      <router-view />
-    </v-main>
-  </div>
+    <div>
+        <div v-if="getStatusLogin && !isLoading">
+            <NavBar v-if="!isLoading && ['Maintenance','Unauthorized','Forbidden','PageNotFound','InternalServerError'].indexOf($route.name) === -1"  v-show="getStatusLogin"/>
+        </div>
+        <v-main v-if="!isLoading">
+            <!-- <BreadCrumbsNew v-if="$route.meta.breadcrumbs" /> -->
+            <router-view />
+        </v-main>
+    </div>
 </template>
 
+
 <script>
-import Navbar from "@/components/Navbar.vue"
+    import NavBar from "./components/Navbar.vue";
+    // import { BreadCrumbsNew } from "@vue-mf/global";
 
-export default {
-  name: "App",
-  components: {
-    Navbar
-  },
-  computed: {
-      navbarHandler: function() {
-        let navbar = localStorage.getItem('navbar')
-        return navbar !== '' ? true : false ;
-      }
-  }
-
-};
+    export default {
+        name: "App",
+        components: {
+            NavBar,
+            // BreadCrumbsNew,
+        },
+        data() {
+            return { isLoading: true };
+        },
+        computed: {
+            getStatusLogin: function () {
+                var valToken = localStorage.getItem('bearer')
+                if(valToken !== ''){
+                    return true
+                }else{
+                    return false
+                }
+            }
+        },
+        mounted() {
+            setTimeout(() => {
+                this.isLoading = false;
+            }, 50);
+        },
+        created() {
+            document.title = 'Dashboard - '+this.$route.meta.title;
+        },
+        watch: {
+            '$route' (to, from) {
+                document.title = to.meta.title ? 'Dashboard - '+ to.meta.title : 'EdenFarm'|| 'EdenFarm'
+            }
+        },
+    };
 </script>
