9758690e73448559e5ac1d5ba5e37532a063561f
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2022-10-26 17:23:31 +0700

    add hide condition with error page

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,53 @@
 <template>
-    <div>
-        <div v-if="getStatusLogin && !isLoading">
-            <NavBar v-if="!isLoading && ['Maintenance','Unauthorized','Forbidden','PageNotFound','InternalServerError'].indexOf($route.name) === -1"  v-show="getStatusLogin"/>
-        </div>
-        <v-main v-if="!isLoading">
-            <router-view />
-        </v-main>
+  <div>
+    <div v-if="getStatusLogin && !isLoading">
+      <NavBar v-if="!isLoading && !getErrorRoute" v-show="getStatusLogin" />
     </div>
+    <v-main v-if="!isLoading">
+      <router-view />
+    </v-main>
+  </div>
 </template>
 
-
 <script>
-    import NavBar from "./components/Navbar.vue";
+import NavBar from "./components/Navbar.vue";
 
-    export default {
-        name: "App",
-        components: {
-            NavBar,
-        },
-        data() {
-            return { isLoading: true };
-        },
-        computed: {
-            getStatusLogin: function () {
-                var valToken = localStorage.getItem('bearer')
-                if(valToken !== ''){
-                    return true
-                }else{
-                    return false
-                }
-            }
-        },
-        mounted() {
-            setTimeout(() => {
-                this.isLoading = false;
-            }, 50);
-        },
-        created() {
-            document.title = 'Dashboard - '+this.$route.meta.title;
-        },
-        watch: {
-            '$route' (to, from) {
-                document.title = to.meta.title ? 'Dashboard - '+ to.meta.title : 'EdenFarm'|| 'EdenFarm'
-            }
-        },
-    };
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+  data() {
+    return { isLoading: true };
+  },
+  computed: {
+    getStatusLogin: function () {
+      var valToken = localStorage.getItem("bearer");
+      if (valToken !== "") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getErrorRoute: function () {
+      let url = window.location.href;
+      return url.includes("503") || url.includes("403");
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 50);
+  },
+  created() {
+    document.title = "Dashboard - " + this.$route.meta.title;
+  },
+  watch: {
+    $route(to, from) {
+      document.title = to.meta.title
+        ? "Dashboard - " + to.meta.title
+        : "EdenFarm" || "EdenFarm";
+    },
+  },
+};
 </script>
