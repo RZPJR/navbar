dec2254a49311e09e1ba2a7c5a51b1d107501da8
Sigi Kemahduta <sigi.kemahduta@edenfarm.id>
2022-10-26 15:56:07 +0700

    fix sockjs ssl

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
       disableHostCheck: true,
       sockPort: 8501,
       sockHost: "localhost",
-      https: true,
+      // https: true,
       port: 8501,
     },
     externals: ["vue", "vue-router", /^@vue-mf\/.+/],
